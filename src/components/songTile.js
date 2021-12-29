2e3debc9dadfb2953f6b57f122121bc4c9f9c049
MierafM <mierafmulat@gmail.com>
2021-12-29 17:43:32 -0600

    added audio to song tiles

--- src/components/songTile.js
+++ src/components/songTile.js
@@ -2,36 +2,46 @@ import React, { Component } from 'react';
 import './songTileStyle.css';
 
 class SongTile extends Component{
-  constructor() {
-    super();
-    this.state = {}
-
+  state = {
+    play: false,
+    preview: new Audio(this.props.song.preview_url)
   }
+
+  togglePlay = () => {
+
+  this.setState({ play: !this.state.play },
+    () => {
+    this.state.play ? this.state.preview.play() : this.state.preview.pause();
+    });
+}
   componentDidMount(){
 
-    console.log('this.props', this.props);
-    fetch('https://api.spotify.com/v1/audio-features/'+this.props.song.id, {
-      headers: {'Authorization': 'Bearer ' + this.props.token}
-    }).then(response => response.json()).then(data =>
-      this.setState({
-        track:data
-      })
-    )
-    console.log("in mount", this.state);
+    //console.log('this.props', this.props);
+
+
 
 
   }
 
 
   render(){
-
+    console.log('props', this.props.song.preview_url);
+    console.log(this.state);
     var link = 'id/'+this.props.song.id
 
     return(
       <div className="songTileContainer">
         <div className="imageContainer">
-          <img className="songImage" src={this.props.song.album.images[1].url} />
+          <img className="songImage" src={this.props.song.album.images[1].url} onClick={this.togglePlay}/>
         </div>
+        {this.props.song.preview_url ?
+          <div className="hp_slide">
+            <div className="hp_range"></div>
+          </div>
+          :
+          <div></div>
+        }
+
         <div className="songInfoContainer">
           <a href={'/id/'+this.props.song.id} className="title">{this.props.song.name}</a>
           <div className="artists">
